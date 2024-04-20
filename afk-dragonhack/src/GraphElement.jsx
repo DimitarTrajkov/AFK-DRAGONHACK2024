b4973918af4d09daabd83120c364b3684993ba69
Petar <cvetkovicpp@gmail.com>
2024-04-20 19:07:57 +0200

    Fixes and adding statistics page

---
+++ afk-dragonhack/src/GraphElement.jsx
@@ -0,0 +1,10 @@
+
+
+export default function GraphElement({text, percentage}) {
+  
+    return (
+        <div className=" bg-[#247BA0]  h-[220px] w-full p-4">
+            <p className="text-center">{text}</p>
+        </div>
+    );
+};
--- afk-dragonhack/src/Stats.jsx
+++ afk-dragonhack/src/Stats.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import GraphElement from "./GraphElement";
 import SideNavigation from "./SideNavigation";
-
+import StatsElement from "./StatsElement";
 const Stats = () => {
   return (
-    <div classname="container h-full w-full" id="Stats">
-      <div className="w-1/5 h-full">
-      <SideNavigation />
+    <div className="container w-auto h-auto bg-[#E8F1F2]">
+    <div className="container mx-auto">
+      <div className="flex">
+        <div className="w-60">
+          <SideNavigation />
+        </div>
+        <div className="p-5 justify-center m-auto">
+            <div className="flex flex-row gap-x-4 p-5">
+                <StatsElement text="Accuracy" percentage="90%" color="bg-[#006494]"/>
+                <StatsElement text="Accuracy" percentage="90%" color="bg-[#247BA0]"/>
+                <StatsElement text="Accuracy" percentage="90%" color="bg-[#0A2463]"/>
+                <StatsElement text="Accuracy" percentage="90%" color="bg-[#022B3A]"/>
+            </div>
+            <div className="grid grid-cols-2 gap-y-4 gap-x-4 p-5">
+                <GraphElement text="Graph" percentage="90%" />
+                <GraphElement text="Graph" percentage="90%" />
+                <GraphElement text="Graph" percentage="90%" />
+                <GraphElement text="Graph" percentage="90%" />
+            </div>
+        </div>
       </div>
     </div>
+</div>
   );
 };
 
