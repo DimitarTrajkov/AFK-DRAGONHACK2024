08ca85de4d218d242e9c812b6a902c7523d2fa17
Dimitar <dt18806@student.uni-lj.si>
2024-04-20 12:43:48 +0200

    added an home page with side navigation

---
+++ afk-dragonhack/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideNavigation from "./SideNavigation";
+
+const Home = () => {
+  return (
+    <div id="Home">
+      <SideNavigation />
+    </div>
+  );
+};
+
+export default Home;
---
+++ afk-dragonhack/src/SideNavigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SideNavigation = () => {
+  return (
+    <div id="SideBar">
+      <p>bla bla</p>
+      <p>d bla</p>
+      <p>f bla</p>
+      <p>g bla</p>
+      <p>ew bla</p>
+    </div>
+  );
+};
+
+export default SideNavigation;
